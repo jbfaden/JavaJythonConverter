38adc423a9c0070ef009d8332b8d21bfbc3f6b6e
Jeremy Faden <faden@cottagesystems.com>
2022-10-19 08:17:25 -0500

    use char to verify that strings are handled properly.

--- src/java/test/LocalInstanceVariable.java
+++ src/java/test/LocalInstanceVariable.java
@@ -6,24 +6,24 @@ package test;
  * @author jbf
  */
 public class LocalInstanceVariable {
-    int vv = 0;
+    char vv = '0';
     
     public void action() {
-        vv= 1;
+        vv= '1';
         System.out.println("vv: "+ vv);
         if ( true ) {
-            int vv = 2;
+            char vv = '2';
             System.out.println("vv: "+ vv);
             System.out.println("this.vv: "+ this.vv);
             if ( true ) {
-                vv = 3;
+                vv = '3';
                 System.out.println("vv: "+ vv);
-                this.vv=4;
+                this.vv='4';
                 System.out.println("this.vv: "+ this.vv);
             }
-            this.vv= 5;
+            this.vv= '5';
             System.out.println("this.vv: "+ this.vv);
-            vv= 6;
+            vv= '6';
             System.out.println("vv: "+ vv);
         }
     }
