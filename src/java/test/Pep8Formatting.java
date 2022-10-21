50ba8d15aa384b3bca41ae6278766caf9f5d7098
Jeremy Faden <faden@cottagesystems.com>
2022-10-21 07:46:21 -0500

    pep8 formatting test

---
+++ src/java/test/Pep8Formatting.java
@@ -0,0 +1,17 @@
+
+package test;
+
+/**
+ *
+ * @author jbf
+ */
+public class Pep8Formatting {
+    public static void main( String[] args ) {
+        int c= 1+2*4/5;
+        if ( c==9 ) {
+            c++;
+        } else {
+            c=c+1;
+        }
+    }
+}
