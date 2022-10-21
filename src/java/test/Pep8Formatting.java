357868eeaa8ad711592d9681cd2541354bf41c0c
Jeremy Faden <faden@cottagesystems.com>
2022-10-21 07:50:47 -0500

    pep8 formatting test

--- src/java/test/Pep8Formatting.java
+++ src/java/test/Pep8Formatting.java
@@ -2,16 +2,18 @@
 package test;
 
 /**
- *
+ * pep8 says there should be whitespace around operators, etc.
  * @author jbf
  */
 public class Pep8Formatting {
     public static void main( String[] args ) {
-        int c= 1+2*4/5;
-        if ( c==9 ) {
+        int c= (1+2)*4/5;
+        if ( c>9 ) {
             c++;
-        } else {
+        } else if (c==8) {
             c=c+1;
+        } else {
+            c=-c;
         }
     }
 }
