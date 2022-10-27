2b44c20f917268064e976859c4b97b8535d1c44c
Jeremy Faden <faden@cottagesystems.com>
2022-10-27 08:47:18 -0500

    demo string builder handling

---
+++ src/java/test/StringBuilderDemo.java
@@ -0,0 +1,16 @@
+package test;
+
+public class StringBuilderDemo {
+    public static void main( String[] args ) {
+        int seconds=12;
+        int nanoseconds= 123456789;
+        StringBuilder sb= new StringBuilder("P");
+        sb.append("T");
+        sb.append(String.format("%.3f",seconds + nanoseconds/1e9) ).append("S");
+        System.out.println(sb.toString());
+        
+        sb= new StringBuilder("Hi Thee");
+        sb.insert(6,"r");
+        System.out.println(sb.toString());
+    }
+}
