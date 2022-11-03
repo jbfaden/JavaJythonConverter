0a2bbbea94a9b72e22055c8121131cff8a57410c
Jeremy Faden <faden@cottagesystems.com>
2022-11-03 15:38:50 -0500

    version

--- src/java/com/cottagesystems/convert/Convert.java
+++ src/java/com/cottagesystems/convert/Convert.java
@@ -96,7 +96,7 @@ import java.util.stream.Collectors;
  */
 public class Convert {
 
-    public static final String VERSION = "20221103a";
+    public static final String VERSION = "20221103b";
     
     public Convert() {
         this.stack = new Stack<>();
