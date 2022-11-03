d9f0126749b21a0649a4bed8b98bd7d9a20d899e
Jeremy Faden <faden@cottagesystems.com>
2022-11-03 16:58:25 -0500

    if we know the method, then we know the return type.

--- src/java/com/cottagesystems/convert/Convert.java
+++ src/java/com/cottagesystems/convert/Convert.java
@@ -96,7 +96,7 @@ import java.util.stream.Collectors;
  */
 public class Convert {
 
-    public static final String VERSION = "20221103b";
+    public static final String VERSION = "20221103c";
     
     public Convert() {
         this.stack = new Stack<>();
