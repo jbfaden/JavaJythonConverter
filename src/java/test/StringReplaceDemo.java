1944fcc951a921d1c5b65cb0f28bd48fd38fc189
Jeremy Faden <faden@cottagesystems.com>
2023-01-05 07:52:20 -0600

    add numbers to make it easier to compare outputs

--- src/java/test/StringReplaceDemo.java
+++ src/java/test/StringReplaceDemo.java
@@ -13,14 +13,16 @@ public class StringReplaceDemo {
     public static void main(String[]args ) {
         String s= "121212[23]4";
         String b= "1234\\\\45";
-        System.err.println( s.replaceFirst("1","7") );
-        System.err.println( b.replaceFirst("[\\\\]{2}","\\\\") ); // these are regular expressions
-        System.err.println( b.replace("\\\\","\\") );  // this is not
-        System.err.println( s.replaceAll("1","7") );
-        System.err.println( s.replace("1","7") );
-        System.err.println( s.replaceFirst("1","7") );
-        System.err.println( s.replace("[23]","_") );
-        System.err.println( s.replace("\\3", "_"));
+        System.err.println( "01: " + s );
+        System.err.println( "02: " + b );
+        System.err.println( "03: " + s.replaceFirst("1","7") );
+        System.err.println( "04: " + b.replaceFirst("[\\\\]{2}","\\\\") ); // these are regular expressions
+        System.err.println( "05: " + b.replace("\\\\","\\") );  // this is not
+        System.err.println( "06: " + s.replaceAll("1","7") );
+        System.err.println( "07: " + s.replace("1","7") );
+        System.err.println( "08: " + s.replaceFirst("1","7") );
+        System.err.println( "09: " + s.replace("[23]","_") );
+        System.err.println( "10: " + s.replace("\\3", "_"));
     }
     
 }
