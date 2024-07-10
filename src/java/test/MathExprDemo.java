b0b1cf14cbc25e21d89606185a6c8b25aee5f1ca
Jeremy Faden <faden@cottagesystems.com>
2024-07-10 12:21:38 -0500

    add another test for math in-line expressions (+,**,etc)

---
+++ src/java/test/MathExprDemo.java
@@ -0,0 +1,17 @@
+
+package test;
+
+/**
+ * demonstrate order of operations, representation of pow, etc.
+ * @author jbf
+ */
+public class MathExprDemo {
+    public static void main( String[] args ) {
+        System.out.println(3+4);
+        System.out.println(3*4);
+        System.out.println(3-4);
+        System.out.println(3/4);
+        System.out.println(3*4+2);
+        System.out.println(Math.pow(3,4));
+    }
+}
