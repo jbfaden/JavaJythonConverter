37dd3c031203276cb765c16c8b32be3f74927c07
Jeremy Faden <faden@cottagesystems.com>
2024-07-31 12:56:10 -0500

    tweak version

--- src/java/com/cottagesystems/convert/ConvertJavaToPython.java
+++ src/java/com/cottagesystems/convert/ConvertJavaToPython.java
@@ -96,7 +96,7 @@ import java.util.stream.Collectors;
  */
 public class ConvertJavaToPython {
 
-    public static final String VERSION = "20240731a";
+    public static final String VERSION = "20240731b";
     
     public ConvertJavaToPython() {
         this.stack = new Stack<>();
