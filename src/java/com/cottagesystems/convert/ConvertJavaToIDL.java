3bb0ae83066c52260104b9738bd27cf86d2ecfb6
Jeremy Faden <faden@cottagesystems.com>
2025-02-17 06:29:58 -0600

    tweak version

--- src/java/com/cottagesystems/convert/ConvertJavaToIDL.java
+++ src/java/com/cottagesystems/convert/ConvertJavaToIDL.java
@@ -94,7 +94,7 @@ import java.util.stream.Collectors;
  */
 public class ConvertJavaToIDL {
 
-    public static final String VERSION = "20240213a";
+    public static final String VERSION = "20240217a";
     
     public ConvertJavaToIDL() {
         this.stack = new Stack<>();
