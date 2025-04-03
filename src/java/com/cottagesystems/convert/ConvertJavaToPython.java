852adc9be7fa7d421e9f7a97cfc8578f6ac4dd9c
Jeremy Faden <faden@cottagesystems.com>
2025-04-03 07:01:21 -0500

    add landing page to new web.xml.  tweak version

--- src/java/com/cottagesystems/convert/ConvertJavaToPython.java
+++ src/java/com/cottagesystems/convert/ConvertJavaToPython.java
@@ -96,7 +96,7 @@ import java.util.stream.Collectors;
  */
 public class ConvertJavaToPython {
 
-    public static final String VERSION = "20240403a";
+    public static final String VERSION = "20240403b";
 
     public ConvertJavaToPython() {
         this.stack = new Stack<>();
